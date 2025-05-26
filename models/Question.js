dff659e71d2abf0853fb45a436067f0c502c42fc
evelyn-zhan <evelynzanpx@gmail.com>
2025-05-26 09:56:24 +0700

    create Question database model

---
+++ models/Question.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+const questionSchema = new mongoose.Schema({
+    question: { type: String, required: true },
+    type: { type: String, required: true },
+    options: { type: [String] },
+    answer: { type: [String], required: true }
+}, { timestamps: false })
+
+const Question = mongoose.model("Question", questionSchema)
+
+export default Question

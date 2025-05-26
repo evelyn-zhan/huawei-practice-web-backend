fe5138e9db9c3b1e57fa05629de599e28a8924cd
evelyn-zhan <evelynzanpx@gmail.com>
2025-05-26 09:20:38 +0700

    add 'role' data to user schema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     username: { type: String, required: true },
     class: { type: String, required: true },
+    role: { type: String, required: true },
     password: { type: String, required: true }
 }, { timestamps: false })
 

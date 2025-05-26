ed8d1037e9058b7099cadc939de4cf52166fed93
evelyn-zhan <evelynzanpx@gmail.com>
2025-05-26 09:33:52 +0700

    update GET /user fail message, update GET /user/:id not found message and fail message, fix logic error in POST /user route for signup

--- routes/user.js
+++ routes/user.js
@@ -9,39 +9,58 @@ router.get("/", async (req, res) => {
         const users = await User.find()
         res.status(200).json(users)
     } catch (error) {
-        res.status(500).json({ message: "Failed to get users", error: error.message })
+        res.status(500).json({
+            message: "Failed to get users.",
+            error: error.message
+        })
     }
 })
 
 // GET user by Id for Login
 router.get("/:id", async (req, res) => {
     try {
-        const user = await User.findById(req.params.id)
-        if (!user) return res.status(404).json({ message: "There is no such user!" })
-        res.status(200).json(user)
+        const user = await User.findOne({ userId: req.params.id })
+        if (!user) {
+            return res.status(404).json({
+                message: "Oops! We couldn't find an account with that User ID."
+            })
+        }
+        res.status(200).json(user)  
     } catch (error) {
-        res.status(500).json({ message: "Failed to get user", error: error.message })
+        res.status(500).json({
+            message: "Failed to get user.",
+            error: error.message
+        })
     }
 })
 
 // POST new user for Signup
 router.post("/", async (req, res) => {
     try {
+        const existingUser = await User.findOne({ userId: req.body.userId })
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User ID already exists!"
+            })
+        }
+
         const newUser = new User(req.body)
         const savedUser = await newUser.save()
-
-        if (User.exists({ userId: savedUser.userId })) return res.status(400).json({ message: "User ID already exists!" })
         
-            res.status(201).json({
-            message: "User created successfully",
+        res.status(201).json({
+            message: "New user created successfully!",
             user: {
                 userId: savedUser.userId,
                 username: savedUser.username,
-                class: savedUser.class
+                class: savedUser.class,
+                role: savedUser.role
             }
         })
     } catch (error) {
-        res.status(500).json({ message: "Failed to create user", error: error.message })
+        res.status(500).json({
+            message: "Failed to create new user.",
+            error: error.message
+        })
     }
 })
 

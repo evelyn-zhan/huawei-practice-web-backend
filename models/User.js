36a1773048e38347777eca99de15e8f068f836b3
evelyn-zhan <evelynzanpx@gmail.com>
2025-06-08 22:20:04 +0700

    add classId to User model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose"
 const userSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     username: { type: String, required: true },
-    class: { type: String, required: true },
+    classId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "StudentClass"
+    },
     role: { type: String, required: true },
     password: { type: String, required: true }
 }, { timestamps: false })

1d5b8bb8b41f95a37545f7601db268dfeb077dd9
evelyn-zhan <evelynzanpx@gmail.com>
2025-06-19 22:36:01 +0700

    create Quiz model

---
+++ models/Quiz.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+const quizSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    questionCount: { type: Number, required: true },
+    questions: { type: [Object], required: true }
+}, { timestamps: false })
+
+const Quiz = mongoose.model("Quiz", quizSchema)
+
+export default Quiz

14cfaec362320baab29f16d3826267fe0d7b73cc
evelyn-zhan <evelynzanpx@gmail.com>
2025-06-26 22:16:20 +0700

    add type and time to Quiz schema

--- models/Quiz.js
+++ models/Quiz.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose"
 const quizSchema = new mongoose.Schema({
     title: { type: String, required: true },
     questionCount: { type: Number, required: true },
-    questions: { type: [Object], required: true }
+    questions: { type: [Object], required: true },
+    type: { type: String, required: true },
+    time: { type: Number, required: true }
 }, { timestamps: false })
 
 const Quiz = mongoose.model("Quiz", quizSchema)
